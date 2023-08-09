91c6e25c9a0cc128b5fe25825847c4a6448efbb0
Kamoliddin-2713 <murotovkamoliddin95@gmail.com>
2023-08-09 17:46:11 +0500

    JS_lesson

---
+++ js_lesson/main.js
@@ -0,0 +1,35 @@
+// console.log("Hello world!")
+// console.log(ism);
+// let son = true
+// console.log(typeof son);
+// let ism = "Kamoliddin"
+// console.log(ism);
+// ism = "Danyor"
+// console.log(ism);
+// const ism = "Kamoliddin"
+// console.log(ism);
+// ism = "Nodir"
+// console.log(ism);
+// let son1 = 30;
+// let son2 = 3;
+// console.log(son1+son2);
+// console.log(son1-son2);
+// console.log(son1*son2);
+// console.log(son1/son2);
+// console.log(son1%son2);
+// console.log(son1++);
+// // console.log(++son2);
+// console.log(son1);
+// console.log(--son1);
+// console.log(son1--);
+// console.log(son1);
+// console.log(son1++);
+// console.log(++son1);
+// console.log(son1++);
+// console.log(++son1);
+let son1 = '30';
+let son2 = '30';
+console.log(son1==son2);
+console.log(son1===son2);
+console.log(son1!=son2);
+console.log(son1!==son2);
