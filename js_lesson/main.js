e566662b992282984eab3f569a9432bf1932018c
Kamoliddin-2713 <murotovkamoliddin95@gmail.com>
2023-08-13 23:18:09 +0500

    masala

--- js_lesson/main.js
+++ js_lesson/main.js
@@ -79,15 +79,51 @@
 // console.log(`a * b = ${a*b} `);
 // console.log(`a / b = ${a/b} `);
 // console.log(`a % b = ${a%b} `);
-let person = {
-    ism: "Nodir",
-    age : 28,
-    from : "Tashkent",
-    salary : 4000000,
-    t:100
-}
+// let person = {
+//     ism: "Nodir",
+//     age : 28,
+//     from : "Tashkent",
+//     salary : 4000000,
+//     t:100
+// }
 
-console.log(person);
-console.log(`
-Ism : ${person.ism},
-oyligi : ${person.salary}`);
+// console.log(person);
+// console.log(`
+// Ism : ${person.ism},
+// oyligi : ${person.salary}`);
+// let a = +prompt("a ga qiymat bering")
+// let b = +prompt("b ga qiymat bering")
+
+// console.log(a + b);
+// console.log(Math.PI);
+// console.log(Math.sqrt(16));
+// console.log(Math.abs(-9));
+// console.log(Math.floor(2.54));
+// console.log(Math.ceil(2.4));
+// console.log(Math.round(5.5));
+// console.log(Math.floor(Math.random()*100));
+// let a = +prompt('a ni kiriting')
+// let b = +prompt('b ni kiriting')
+// console.log(Math.sqrt(a*b));
+// console.log(`natija : ${Math.sqrt(a+b)}`);.
+// let a = prompt("Sevimli futbol komandangiz nomini kiriting:")
+// console.log(`${a} CHEMPION`);
+// let a = prompt("Ismingiz?")
+// console.log(`Assalom aleykum ${a}`);
+// let a = +prompt('Raqam kiriting:')
+// console.log(`
+// Kiritilgan raqam ${a},
+// ${a} dan oldin ${a-1},
+// ${a} dan keyin ${a+1}`);
+// let nok = prompt('dokonda  1 kg nok  5000 somdan.Necha kg olasiz ?: ')
+// let sabzi = prompt('dokonda  1 kg sabzi  3000 somdan.Necha kg olasiz ?: ')
+// let bodring = prompt('dokonda  1 kg bodring  2000 somdan.Necha kg olasiz ?: ')
+//  console.log(`
+// Siz  ${nok} kg nok oldingiz ${nok*5000} so'm
+// Siz  ${sabzi} kg sabzi oldingiz ${sabzi*3000} so'm
+// Siz  ${bodring} kg bodring oldingiz ${bodring*2000} so'm
+// Jami sizdan : ${nok*5000 + sabzi*3000+bodring*2000} so'm
+// `)
+// let a = +prompt('a ni kiritig:')
+// let b = +prompt('b ni kiriting')
+// console.log(`a va b ni orta arifmetigi ${(a+b)/2}`);
