6a1e2b64faf5d73a8a60857b0cc1212a96b73459
Kamoliddin-2713 <murotovkamoliddin95@gmail.com>
2023-08-13 12:36:34 +0500

    object and massiv

--- js_lesson/main.js
+++ js_lesson/main.js
@@ -41,3 +41,22 @@
 // // son1/=son2
 // son1%=son2
 // console.log(son1);
+// let ism = 15
+// let i = 8
+// console.log(i);
+// console.log(ism);
+// i = 7.7
+// let m = "Nodir"
+// console.log(m);
+// let t = true
+// console.log(t);
+// let n =  {
+//     ism : "Bobur",
+//     yosh : 25
+// }
+// console.log(n);
+// let t = [1,"test",{
+//     ism:"Bobur",
+//     yosh:25
+// }]
+// console.log(t);
